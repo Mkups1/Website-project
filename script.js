9060c3319065f004463133b353abeb403c978337
Mkups1 <m.kupilik1@share.epsb.ca>
2022-03-10 13:25:09 -0700

    Started quiz

---
+++ script.js
@@ -0,0 +1,14 @@
+// Quiz javascript
+
+let question1 = document.getElementById("q1")
+let question2 = document.getElementById("q2")
+let question3 = document.getElementById("q3")
+let question4 = document.getElementById("q4")
+let question5 = document.getElementById("q5")
+
+// Event listener
+document.getElementById("button").addEventListener("click", submit)
+// Event function
+function submit(){
+    alert("hello")
+}
